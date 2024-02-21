c9cf8f2e9ae0a4257a27590d2e8e499f614f3bb2
prahaladbelavadi <github@belavadi.com>
2024-02-20 16:14:22 -0800

    scaffold basics

---
+++ components/ListOfProjects.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Project from './Project';
+
+const ListOfProjects = () => {
+    return (
+        <View>
+            <Text>ListOfProjects</Text>
+            <Project />
+        </View>
+    )
+}
+
+export default ListOfProjects
